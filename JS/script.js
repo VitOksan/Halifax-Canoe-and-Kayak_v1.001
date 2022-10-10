02f3b514c424239bcb2e01b8f6670c4c29f4b450
VitOksan <106928417+VitOksan@users.noreply.github.com>
2022-10-10 00:01:54 -0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,24 @@
+// console.log('jQuery testing')
+
+//     $('.header__text').click(function() {
+//         alert('Text is clicked');
+//     });
+
+
+// $(document).ready(function(){
+//     $(".header__burger").click(function(){
+//       $(".nav__menu").slideToggle("slow");
+//     });
+//   });
+
+  $(document).ready(function(){
+    $(".header__burger").click(function(){
+      $(".nav__menu").slideToggle();
+    });
+  });
+
+  // $(document).ready(function(){
+  //   $("#flip").click(function(){
+  //     $("#panel").slideToggle("slow");
+  //   });
+  // });
